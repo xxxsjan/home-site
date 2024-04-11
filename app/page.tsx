3f3610be28c2ed42c88c5a8d51d02c7ddae01a0e
xxxsjan <626653354@qq.com>
2024-04-11 20:38:22 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
-// import { Link } from "@nextui-org/link";
+"use client";
 import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
-import { Link, Button, Card, CardBody } from "@nextui-org/react";
+import {
+  Link,
+  Button,
+  Card,
+  CardBody,
+  Listbox,
+  ListboxItem,
+} from "@nextui-org/react";
+
 export default function Home() {
   const siteInfo = [
     {
@@ -16,19 +24,24 @@ export default function Home() {
       name: "前端工具",
       href: "https://web-tool.dolam.fun",
     },
+    {
+      name: "崩铁助手",
+      href: "https://star-rail.dolam.fun",
+    },
+    // {
+    //   name: "原神素材",
+    //   href: "https://genshin-home.vercel.app/",
+    // },
   ];
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-lg text-center justify-center">
-        {/* <h1 className={title()}>Hello&nbsp;</h1> */}
-        {/* <h1 className={title({ color: "violet" })}>Hello&nbsp;</h1> */}
-        <Card>
-          <CardBody>
-            <div className="flex gap-4">
-              {siteInfo.map((item, index) => {
-                return (
+        <div className="w-full max-w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100">
+          <Listbox aria-label="Actions" onAction={() => {}}>
+            {siteInfo.map((item) => {
+              return (
+                <ListboxItem key={item.name}>
                   <Link
-                    key={index}
                     isExternal
                     href={item.href}
                     showAnchorIcon
@@ -36,29 +49,14 @@ export default function Home() {
                   >
                     {item.name}
                   </Link>
-                );
-              })}
-            </div>
-          </CardBody>
-        </Card>
-
-        {/* <br /> */}
-        {/* <h1 className={title()}>
-					websites regardless of your design experience.
-				</h1> */}
-        {/* <h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
-				</h2> */}
+                </ListboxItem>
+              );
+            })}
+          </Listbox>
+        </div>
       </div>
 
       <div className="flex gap-3">
-        {/* <Link
-					isExternal
-					href={siteConfig.links.docs}
-					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
-				>
-					Documentation
-				</Link> */}
         <Link
           isExternal
           className={buttonStyles({ variant: "bordered", radius: "full" })}
