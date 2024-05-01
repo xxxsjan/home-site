c3c5ee3f2873cf05e065bef8dfbb72cf41c5a503
xxxsjan <626653354@qq.com>
2024-05-02 00:03:03 +0800

    更新了快捷链接和定制样式的代码

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,8 @@ import {
   CardBody,
   Listbox,
   ListboxItem,
+  CardHeader,
+  Image,
 } from "@nextui-org/react";
 
 export default function Home() {
@@ -19,43 +21,41 @@ export default function Home() {
     {
       name: "前端文档库",
       href: "https://blog.dolam.fun",
+      src: "/blog.png",
     },
     {
       name: "前端工具",
       href: "https://web-tool.dolam.fun",
+      src: "/web-tool.png",
     },
-    // {
-    //   name: "崩铁助手",
-    //   href: "https://star-rail.dolam.fun",
-    // },
-    // {
-    //   name: "原神素材",
-    //   href: "https://genshin-home.vercel.app/",
-    // },
   ];
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-lg text-center justify-center">
-        <div className="w-full max-w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100">
-          <Listbox aria-label="Actions" onAction={() => {}}>
-            {siteInfo.map((item) => {
-              return (
-                <ListboxItem key={item.name}>
-                  <Link
-                    isExternal
-                    href={item.href}
-                    showAnchorIcon
-                    color="success"
-                  >
-                    {item.name}
-                  </Link>
-                </ListboxItem>
-              );
-            })}
-          </Listbox>
-        </div>
+        <div className="w-full max-w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100"></div>
       </div>
+      <div className="w-full flex gap-4 flex-wrap justify-center">
+        {siteInfo.map((item) => (
+          <Card className="py-4" key={item.name}>
+            <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+              <p className="text-tiny uppercase font-bold">{item.name}</p>
 
+              {/* <small className="text-default-500">12 Tracks</small> */}
+              {/* <h4 className="font-bold text-large">Frontend Radio</h4> */}
+            </CardHeader>
+            <CardBody className="overflow-visible py-2">
+              <Link isExternal href={item.href} color="success">
+                <Image
+                  alt="Card background"
+                  className="object-cover rounded-xl"
+                  src={item.src}
+                  width={270}
+                />
+              </Link>
+            </CardBody>
+          </Card>
+        ))}
+      </div>
       <div className="flex gap-3">
         <Link
           isExternal
